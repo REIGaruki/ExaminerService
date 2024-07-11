19a243889321c7f938a98fdf713789ab41d9ea0c
REIGaruki <gyakujo@mail.ru>
2024-07-12 00:14:13 +0300

    Initial commit

---
+++ src/main/java/pro/sky/examiner/ExaminerApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.examiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaminerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaminerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/examiner/ExaminerApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.examiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
